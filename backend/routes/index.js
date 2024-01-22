666e09b8374a67a209f3ec0d69f3b95d17e8ffa9
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 13:18:35 +0530

    backend - Account Router added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const userRouter = require('./user.routes')
+const accountRouter = require('./account.routes')
 
 const router = Router()
 
 router.use('/user', userRouter)
+router.use('/account', accountRouter)
 
 module.exports = router
