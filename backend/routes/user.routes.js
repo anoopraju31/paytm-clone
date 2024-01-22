8a145fdf2b3e576f2be9b902c269ab7770175cb3
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 13:08:41 +0530

    backend - user router clean up

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,10 +9,6 @@ const { authMiddleware } = require('../middlewares/auth.middlewares')
 
 const router = Router()
 
-router.get('/', (req, res) => {
-	res.json({ message: 'api v1 user router' })
-})
-
 router.post('/sign-up', signUpController)
 router.post('/sign-in', signInController)
 router.put('/', authMiddleware, updateController)
