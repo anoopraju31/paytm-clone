b13e676bd2b9af33995e23c7aa8f3c4987a8b82b
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 12:52:09 +0530

    backend - input validations refactor

--- backend/helpers/validations.js
+++ backend/helpers/validations.js
@@ -1,18 +1,42 @@
 const zod = require('zod')
 
+const emailSchema = zod
+	.string()
+	.min(1, { message: 'Email is required.' })
+	.email('Invalid email.')
+
+const firstNameSchame = zod
+	.string()
+	.min(1, { message: 'First name is required.' })
+
+const lastNameSchame = zod
+	.string()
+	.min(1, { message: 'Last name is required.' })
+
+const passwordSchema = zod
+	.string()
+	.min(8, { message: 'Password must contain atleast 8 characters.' })
+
 const signUpSchema = zod.object({
-	username: zod.string().email(),
-	firstName: zod.string(),
-	lastName: zod.string(),
-	password: zod.string(),
+	username: emailSchema,
+	firstName: firstNameSchame,
+	lastName: lastNameSchame,
+	password: passwordSchema,
 })
 
 const signInSchema = zod.object({
-	username: zod.string().email(),
-	password: zod.string(),
+	username: emailSchema,
+	password: passwordSchema,
+})
+
+const updateSchema = zod.object({
+	firstName: firstNameSchame.optional(),
+	lastName: lastNameSchame.optional(),
+	password: passwordSchema.optional(),
 })
 
 module.exports = {
 	signUpSchema,
 	signInSchema,
+	updateSchema,
 }
