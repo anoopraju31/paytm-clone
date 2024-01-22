0ee7dbcb16c2d49c09fa6ee6208873915c4e8435
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 22:15:06 +0530

    Frontend - Balance Compoment - style change

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -5,12 +5,10 @@ const Balance = () => {
 	const balance = useRecoilValueLoadable(balanceAtom)
 	if (balance.state !== 'hasValue') return null
 	return (
-		<section className='border-t border-orange-50 p-10 flex justify-center items-center'>
-			<div className='w-full max-w-4xl'>
-				<h2 className='font-mono font-bold text-xl text-orange-800'>
-					Your Balance: {balance.contents}
-				</h2>
-			</div>
+		<section className='border-t border-orange-800 p-10'>
+			<h2 className='font-mono font-bold text-xl text-orange-800'>
+				Your Balance: {balance.contents}
+			</h2>
 		</section>
 	)
 }
