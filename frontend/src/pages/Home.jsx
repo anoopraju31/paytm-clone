58c81a99aa15152f6b0aa283f3150669d13d897a
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 17:59:02 +0530

    Frontend - auth state added to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import { useRecoilValue } from 'recoil'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { authAtom } from '../store/atom'
+
 const Home = () => {
+	const auth = useRecoilValue(authAtom)
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (!auth) navigate('/sign-in')
+	}, [navigate, auth])
+
 	return <div>Home</div>
 }
 
