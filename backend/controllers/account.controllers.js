60d8a17462124c49bf922e9bf8fcc81aa8f3f615
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 13:55:30 +0530

    backend - account balance route added

--- backend/controllers/account.controllers.js
+++ backend/controllers/account.controllers.js
@@ -1,5 +1,11 @@
+const Account = require('../models/acount.models')
+
 const balanceController = async (req, res) => {
-	res.json({ message: 'balance route' })
+	const account = await Account.findOne({ userId: req.userId })
+
+	if (!account) return res.status(402).json({ message: 'Account not found' })
+
+	res.json({ balance: account.balance })
 }
 
 module.exports = {
