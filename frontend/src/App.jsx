e09535d3711278e5e183b66bc1e08e062798413c
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 15:43:13 +0530

    Frontend - Home Page - initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import Home from './pages/Home'
 
 const App = () => {
 	return (
@@ -8,6 +9,7 @@ const App = () => {
 			<Routes>
 				<Route path='/sign-up' element={<SignUp />} />
 				<Route path='/sign-in' element={<SignIn />} />
+				<Route path='/' element={<Home />} />
 			</Routes>
 		</BrowserRouter>
 	)
