356622ef0bfb1241bfc1da4a2641a19c533ebbf7
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 17:17:45 +0530

    Frontend - Sign In Page - checkIsAuthenticated helper added

--- frontend/src/helpers/index.js
+++ frontend/src/helpers/index.js
@@ -1,1 +1,20 @@
+import { jwtDecode } from 'jwt-decode'
+
 export const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export const checkIsAuthenticated = () => {
+	const token = localStorage.getItem('token')
+
+	if (!token) return false
+
+	try {
+		const decodedToken = jwtDecode(token)
+
+		if (decodedToken.exp * 1000 > Date.now()) return true
+		else localStorage.removeItem('token')
+	} catch (error) {
+		console.error('Invalid token format')
+		localStorage.removeItem('token')
+		return false
+	}
+}
