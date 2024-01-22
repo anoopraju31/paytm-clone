31288cc24bab241d3bfed04375573127be679535
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 16:13:43 +0530

    Frontend - Header Component - initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Home from './pages/Home'
@@ -6,6 +7,7 @@ import Home from './pages/Home'
 const App = () => {
 	return (
 		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route path='/sign-up' element={<SignUp />} />
 				<Route path='/sign-in' element={<SignIn />} />
