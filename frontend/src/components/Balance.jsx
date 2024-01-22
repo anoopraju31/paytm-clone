d288571f1a4e3432af55fe80eab859296c30d475
anoopraju31 <anoopraju31@gmail.com>
2024-01-23 00:24:14 +0530

    Frontend - Balance component - refactor

--- frontend/src/components/Balance.jsx
+++ frontend/src/components/Balance.jsx
@@ -9,7 +9,7 @@ const Balance = () => {
 	return (
 		<section className='border-t border-orange-800 py-6 px-4 sm:p-10'>
 			<h2 className='p-6 font-mono font-bold text-xl text-orange-800 bg-orange-200 rounded-xl'>
-				Your Balance: {balance.contents}
+				Your Balance: &#8377;{balance.contents / 100}
 			</h2>
 		</section>
 	)
