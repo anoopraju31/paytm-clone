65f3f7cc65590e67f10d7fd59a2c517a3702228a
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 21:49:24 +0530

    Frontend - Header Compoment refactor

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,51 +1,44 @@
-import { useRecoilState } from 'recoil'
-import { memo, useCallback, useState } from 'react'
+import { useRecoilStateLoadable } from 'recoil'
+import { useCallback, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaRegCircleUser } from 'react-icons/fa6'
-import PropTypes from 'prop-types'
 import Button from '../components/Button'
 import { authAtom } from '../store/atom'
 
 const Header = () => {
-	const [showUserMenu, setShowUserMenu] = useState(false)
-	const [auth, setAuth] = useRecoilState(authAtom)
-
-	const toggleUserMenu = useCallback(() => setShowUserMenu((prev) => !prev), [])
-	const logout = useCallback(() => {
-		localStorage.setItem('token', null)
-		setAuth(false)
-	}, [setAuth])
-
 	return (
 		<header className='fixed top-0 left-0 w-full p-5 md:px-10 font-mono backdrop-blur-lg flex justify-between items-center'>
 			<div className='text-xl md:text-2xl font-bold text-orange-900'>
 				<Link to='/'> PayTM </Link>
 			</div>
 
-			{auth && (
-				<div
-					onClick={toggleUserMenu}
-					className='text-2xl relative text-orange-900'>
-					<FaRegCircleUser />
-					{showUserMenu && <UserMenu logout={logout} />}
-				</div>
-			)}
+			<UserProfile />
 		</header>
 	)
 }
 
-const UserMenu = memo(({ logout }) => {
+const UserProfile = () => {
+	const [showUserMenu, setShowUserMenu] = useState(false)
+	const [auth, setAuth] = useRecoilStateLoadable(authAtom)
+
+	const toggleUserMenu = useCallback(() => setShowUserMenu((prev) => !prev), [])
+	const logout = () => {
+		localStorage.removeItem('token')
+		setAuth(null)
+	}
+
+	if (auth.state !== 'hasValue' || !auth.contents) return null
+
 	return (
-		<div className='p-2 flex justify-center items-center bg-orange-300 rounded-lg absolute top-12 right-0'>
-			<Button title='Logout' handleClick={logout} />
+		<div onClick={toggleUserMenu} className='text-2xl relative text-orange-900'>
+			<FaRegCircleUser />
+			{showUserMenu ? (
+				<div className='p-2 flex justify-center items-center bg-orange-300 rounded-lg absolute top-12 right-0'>
+					<Button title='Logout' handleClick={logout} />
+				</div>
+			) : null}
 		</div>
 	)
-})
-
-UserMenu.propTypes = {
-	logout: PropTypes.func,
 }
-
-UserMenu.displayName = 'UserMenu'
 
 export default Header
