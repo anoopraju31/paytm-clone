bcc9cdd13a571deaf819d2aed57366af944afd20
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 16:20:58 +0530

    Frontend - Header Component - style change

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
 	return (
-		<header className='fixed top-0 left-0 w-full p-5 md:px-10 font-mono'>
+		<header className='fixed top-0 left-0 w-full p-5 md:px-10 font-mono backdrop-blur-lg'>
 			<div className='text-xl md:text-2xl font-bold text-orange-900'>
 				<Link to='/'> PayTM </Link>
 			</div>
