f8de0422d00b75376a2da36b3770b80e179cce10
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 16:07:06 +0530

    Frontend - Sign Up Page - style change

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -46,7 +46,7 @@ const SignUp = () => {
 					<Button title='Sign Up' type='submit' />
 				</form>
 
-				<div className='text-center text text-orange-700'>
+				<div className='text-center text-sm text-orange-700 py-2 px-6 hover:bg-orange-600 hover:text-orange-100 rounded-lg transition-colors duration-300 ease-in'>
 					<Link to='/sign-in'> Already have an account? </Link>
 				</div>
 			</section>
