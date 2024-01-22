8d5f5a3acd807ab954ab73c20af64d87b4a67b27
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 16:17:24 +0530

    Frontend - Header Component - styled

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<header className='fixed top-0 left-0 w-full p-5 md:px-10 font-mono'>
+			<div className='text-xl md:text-2xl font-bold text-orange-900'>
+				<Link to='/'> PayTM </Link>
+			</div>
+		</header>
+	)
 }
 
 export default Header
