45722d0785b92bf048bcc705934222517fe1b335
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 15:38:29 +0530

    Frontend - Router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 const App = () => {
 	return (
-		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<h1> Home </h1>} />
+				<Route path='/about' element={<h1> About </h1>} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
