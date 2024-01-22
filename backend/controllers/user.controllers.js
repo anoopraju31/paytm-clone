50c100da4f3013c81e8d43044ea7a1ffd52379e1
anoopraju31 <anoopraju31@gmail.com>
2024-01-22 12:00:53 +0530

    backend - API V1 user sign up route - initial

---
+++ backend/controllers/user.controllers.js
@@ -0,0 +1,7 @@
+const signUpController = async (req, res) => {
+	res.json({ message: 'sign up route' })
+}
+
+module.exports = {
+	signUpController,
+}
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require('express')
+const { signUpController } = require('../controllers/user.controllers')
+
 const router = Router()
 
 router.get('/', (req, res) => {
 	res.json({ message: 'api v1 user router' })
 })
+
+router.post('/sign-up', signUpController)
 
 module.exports = router
